1b394a0e4fee928bd8fb29ff9a1d09953e691ac4
AndersonVTA <andersonventosilla0@gmail.com>
2023-07-02 20:19:14 -0500

    finished

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { createPool } from "mysql2/promise";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+const pool = createPool({
+  host: process.env.MYSQLDB_HOST,
+  user: "root",
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  port: process.env.MYSQLDB_DOCKER_PORT,
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result[0]);
+});
+
+app.listen(process.env.NODE_DOCKER_PORT);
+console.log("server on port", process.env.NODE_DOCKER_PORT);
